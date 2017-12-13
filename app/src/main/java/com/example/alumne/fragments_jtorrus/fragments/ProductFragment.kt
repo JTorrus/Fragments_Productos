3a00d64f4217142f1c198344246d85c25417bd7e
JTorrus <jtorrusgalindo@gmail.com>
2017-12-13 20:51:16 +0100

    Values files implemented, layouts edited

--- app/src/main/java/com/example/alumne/fragments_jtorrus/fragments/ProductFragment.kt
+++ app/src/main/java/com/example/alumne/fragments_jtorrus/fragments/ProductFragment.kt
@@ -10,5 +10,5 @@ import com.example.alumne.fragments_jtorrus.R
 class ProductFragment: ListFragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater!!.inflate(R.layout.fragment_product, container, false)
-    };
+    }
 }
