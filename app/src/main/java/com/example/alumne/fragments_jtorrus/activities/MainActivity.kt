6f60b74ed50872b5d056ac5de9046cb94a635f71
JTorrus <jtorrusgalindo@gmail.com>
2017-12-13 21:00:55 +0100

    Fragment and Main class code started

--- app/src/main/java/com/example/alumne/fragments_jtorrus/activities/MainActivity.kt
+++ app/src/main/java/com/example/alumne/fragments_jtorrus/activities/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.alumne.fragments_jtorrus.activities
 
+import android.app.FragmentManager
+import android.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.alumne.fragments_jtorrus.R
+import com.example.alumne.fragments_jtorrus.fragments.ProductFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = this.fragmentManager
+        val fragmentTrans: FragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTrans.replace(R.id.containerPanel, ProductFragment())
+        fragmentTrans.commit()
     }
 }
--- app/src/main/java/com/example/alumne/fragments_jtorrus/fragments/ProductFragment.kt
+++ app/src/main/java/com/example/alumne/fragments_jtorrus/fragments/ProductFragment.kt
@@ -10,5 +10,7 @@ import com.example.alumne.fragments_jtorrus.R
 class ProductFragment: ListFragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater!!.inflate(R.layout.fragment_product, container, false)
+
+
     }
 }
