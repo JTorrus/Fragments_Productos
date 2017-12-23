214bed6add756d090e5f182f4de3bbed11251bfc
JTorrus <jtorrusgalindo@gmail.com>
2017-12-23 22:00:16 +0100

    methods for both layouts implemented, default key position in tablet mode WIP

--- app/src/main/java/com/example/alumne/fragments_jtorrus/activities/MainActivity.kt
+++ app/src/main/java/com/example/alumne/fragments_jtorrus/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.alumne.fragments_jtorrus.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.alumne.fragments_jtorrus.R
 import com.example.alumne.fragments_jtorrus.fragments.ElementsFragment
 import com.example.alumne.fragments_jtorrus.fragments.SingleElementFragment
@@ -10,20 +11,29 @@ import com.example.alumne.fragments_jtorrus.model.Smartphone
 
 class MainActivity : AppCompatActivity(), OnPhoneSelectedListener {
     override fun onPhoneSelected(smartphone: Smartphone) {
+        val original: SingleElementFragment = supportFragmentManager.findFragmentById(R.id.single_smartphone_fragment) as SingleElementFragment
         val fragmentTransaction: android.support.v4.app.FragmentTransaction = supportFragmentManager.beginTransaction()
         val singleElementFragment = SingleElementFragment.newInstance(smartphone)
 
-        fragmentTransaction.replace(R.id.containerPanel, singleElementFragment).addToBackStack("stack")
-        fragmentTransaction.commit()
+        if (original != null) {
+            fragmentTransaction.replace(R.id.single_smartphone_fragment, singleElementFragment)
+            fragmentTransaction.commit()
+        } else {
+            fragmentTransaction.replace(R.id.containerPanel, singleElementFragment).addToBackStack(null)
+            fragmentTransaction.commit()
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fragmentTransaction: android.support.v4.app.FragmentTransaction = supportFragmentManager.beginTransaction()
-        val elementsFragment = ElementsFragment()
 
-        fragmentTransaction.add(R.id.containerPanel, elementsFragment)
-        fragmentTransaction.commit()
+        if (findViewById<View>(R.id.containerPanel) != null) {
+            val fragmentTransaction: android.support.v4.app.FragmentTransaction = supportFragmentManager.beginTransaction()
+            val elementsFragment = ElementsFragment()
+
+            fragmentTransaction.add(R.id.containerPanel, elementsFragment)
+            fragmentTransaction.commit()
+        }
     }
 }
